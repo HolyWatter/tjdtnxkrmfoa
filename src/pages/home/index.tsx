81e9ea36ae6fd0741384b7b5adfbe286dff9c3f0
HolyWatter <qktjdtn97@icloud.com>
2023-12-14 22:42:16 +0900

    apply cicd

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,8 +3,7 @@ import HomeContents from "components/home/HomeContents";
 const Home = () => {
   return (
     <>
-      <h1>cicd 배포!!</h1>
-      <HomeContents />;
+      <HomeContents />
     </>
   );
 };
