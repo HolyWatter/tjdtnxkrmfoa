2d87ce6d2be31ac342502107c00b1be86c7e3453
HolyWatter <qktjdtn97@icloud.com>
2024-01-11 17:52:03 +0900

    fix : 프로필컴포넌트 수정, 설명란 \n 적용 안되는 문제 해결, 깃헙연결링크 추가

--- src/components/shared/Profile.tsx
+++ src/components/shared/Profile.tsx
@@ -1,3 +1,4 @@
+import GithubIcon from "components/svg/github-icon";
 import useBlogInfo from "hooks/useBlogInfo";
 
 const Profile = () => {
@@ -5,16 +6,26 @@ const Profile = () => {
 
   return (
     <div className="flex flex-col w-full px-5 py-10 border-border border-2 rounded-md gap-5 xs:flex-row xs:p-3 xs:border-b-2 xs:border-x-0 xs:border-t-0 xs:rounded-none">
-      <img
-        alt=""
-        src={blogInfo?.thumbnailUrl}
-        className="bg-gray-500 w-[220px] h-[220px] rounded-full m-auto xs:w-1/4 xs:h-[90px] xs:max-h-[100px] xs:m-0"
-      />
+      <div className="relative">
+        <img
+          alt=""
+          src={blogInfo?.thumbnailUrl}
+          className="bg-gray-500 w-[220px] h-[220px] rounded-full m-auto xs:w-1/4 xs:h-[90px] xs:max-h-[100px] xs:m-0"
+        />
+        <a
+          href="https://github.com/HolyWatter"
+          target="_blank"
+          className="absolute bottom-[-5px] right-5"
+          rel="noreferrer"
+        >
+          <GithubIcon />
+        </a>
+      </div>
       <div className="max-w-full break-all mt-5 xs:w-3/4 xs:mt-0">
         <p className="text-gray text-gray-700 font-semibold dark:text-gray-300 xs:m-0">
           {blogInfo?.nickname}
         </p>
-        <p className="mt-3 text-gray-400 xs:mt-0 xs:text-sm">
+        <p className="mt-3 text-gray-400 xs:mt-0 xs:text-sm whitespace-pre-line">
           {blogInfo?.description}
         </p>
       </div>
